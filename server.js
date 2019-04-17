88364857be4f46aed5806351d064bb4564d6888b
kbailly <baill@LAPTOP-27G5D3KR>
2019-04-18 00:17:11 +0200

    modified for openshift

---
+++ server.js
@@ -0,0 +1,15 @@
+﻿var $tw = require("./boot/boot.js").TiddlyWiki();
+
+$tw.boot.argv = [
+  process.env.OPENSHIFT_DATA_DIR,
+  "--verbose",
+  "--server",
+  process.env.OPENSHIFT_NODEJS_PORT,
+  "$:/core/save/all",
+  "text/plain",
+  "text/html",
+  "<username>",
+  "<password>",
+  process.env.OPENSHIFT_NODEJS_IP,
+];
++$tw.boot.boot();
